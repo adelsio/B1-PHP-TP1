25513567ebc64b35e35686ebf06d55c83926ac5b
adelsio <144631299+adelsio@users.noreply.github.com>
2023-09-27 17:07:04 +0200

    Add files via upload

--- tarifReduitGroupe.php
+++ tarifReduitGroupe.php
@@ -4,8 +4,8 @@
 	
 	echo "Combien y'a t'il d'adultes ?:\n" ;
 	$adulte= fgets ( STDIN) ;
-	$r=intval(23%7) ;
-	$q=(23/7);
+	$r=intval ($adulte%7) ;
+	$q=intval ($adulte/7) ;
 	$prix = $q*7*15+$r*18;
 	echo " Prix a payer : $prix € " ;
 
